ba711ab5db8eef56615a9ce7c78d5125091802dc
doomscourge416 <jet_7@list.ru>
2025-05-27 22:04:27 +0300

    redacted starting scripts + readme updated

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+echo "Установка зависимостей..."
+npm install
+
+echo "Выполнение миграций базы данных..."
+npx sequelize-cli db:migrate
+
 echo "Запуск Docker Compose..."
 docker-compose up -d
 
-echo "Запуск бэкенда (npm run dev)..."
+echo "Запуск бэкенда ..."
 npm run dev &
 
-echo "Запуск фронтенда (npm start)..."
+echo "Запуск фронтенда ..."
 cd client && npm start
